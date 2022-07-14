cc0f1e6d364f8fb807d6d72450abee840926dd82
Hassanabbaass <103119353+Hassanabbaass@users.noreply.github.com>
2022-07-14 15:39:31 +0300

    Navbar initial

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+ 
+  Routes
+} from 'react-router-dom';
+
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Services from './pages/Service/Services';
+import Testimonial from './pages/Testimonial/Testimonial';
+import Navbar from './Components/Navbar/Navbar';
+
+const App = () => {
+  return (
+   <Router>
+    <Navbar/>
+   
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+  
+       <Route exact path="/about" element={<About/>} />
+        
+        <Route exact path="/service" element={<Services/>} />
+       
+       <Route exact path="/testimonial" element={<Testimonial/>} />
+        
+        <Route exact path="/contact" element={<Contact/>} />
+        
+        
+      </Routes>
+    
+   </Router>
+  );
+}
+
+export default App;
